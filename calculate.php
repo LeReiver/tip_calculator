33262966bd3028c1672aa8ead2e0494e07d479ae
Michael Le-Reiver <m.reiver@gmail.com>
2016-10-11 10:33:21 -0700

    First commit

---
+++ calculate.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 10/1/15
+ * Time: 8:12 PM
+ */
+
+$bill = $_GET['bill'];
+$percent = $_GET['percent'];
+$number = $_GET['number'];
+$tip = $bill * $percent;
+$subtotal = $bill + $tip;
+
+if ($number !=0 ) {
+    $total = ($bill + $tip) / $number;
+} else {
+    $number = 1;
+    $total = ($bill + $tip) / $number;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> A Tip Calculator</title>
+        <link rel="stylesheet" type="text/css" href="calculator.css">
+    </head>
+        <body>
+            <div id="content">
+                <h1>A Tip Calculator</h1>
+                <p><table id="table">
+                    <tr>
+                        <td class="left">Bill Amount:&nbsp;</td>
+                        <td class="right">$<?php echo number_format($bill, 2); ?></td>
+                    </tr>
+                    <tr>
+                        <td class="left">Tip Percentage:&nbsp;</td>
+                        <td class="right"><?php echo ($percent * 100); ?> %</td>
+                    </tr>
+                    <tr>
+                        <td class="left">Tip Total:&nbsp;</td>
+                        <td class="right">$<?php echo number_format($tip, 2); ?></td>
+                    </tr>
+                    <td class="left">Bill Subtotal:&nbsp;</td>
+                    <td class="right">$<?php echo number_format($subtotal, 2); ?></td>
+                    <tr>
+                        <td class="left">Number In Party:&nbsp;</td>
+                        <td class="right"><?php echo ($number); ?></td>
+                    </tr>
+                <td class="left"><h4>Bill Total, Each Person:&nbsp;</h4></td>
+                <td class="right"><h3>$<?php echo number_format($total, 2); ?></h3></td>
+                </table>
+                </p>
+            </div>
+        </body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>A Tip Calculator</title>
+        <link rel="stylesheet" type="text/css" href="calculator.css">
+    </head>
+    <body>
+    <div id="content">
+        <h1>A Tip Calculator</h1>
+            <form method="get" action="calculate.php">
+                <table id="table">
+                    <tr>
+                        <td class="left">Enter complete bill amount&nbsp;</td>
+                        <td><input type="text" name="bill"></td>
+                    </tr><tr>
+                        <td class="left">Spitting? Enter number of people&nbsp;</td>
+                        <td style="height:1em;"><input type="text" name="number"></td>
+                    </tr>
+                    <tr>
+                        <td class="left">Choose your tip percentage&nbsp;</td>
+                        <td class="selector"><select type="select" name="percent" style="text-align:center">
+                                <option value="0.20">20%</option>
+                                <option value="0.18">18%</option>
+                                <option value="0.15">15%</option>
+                                <option value="0.10">10%</option>
+                                <option value="0">No tip</option>
+                            </select></td>
+                    </tr>
+                    <tr><td></td><td><input type="submit" value ="SUBMIT"></td></tr>
+                </table>
+            </form>
+    </div>
+    </body>
+</html>
